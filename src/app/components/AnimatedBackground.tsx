efa0a7c40bc98e3ad370a18334fcc91a5e590bf5
Aryan Kuril <160471508+aryankuril@users.noreply.github.com>
2025-06-12 11:50:57 +0530

    project added

--- src/app/components/AnimatedBackground.tsx
+++ src/app/components/AnimatedBackground.tsx
@@ -5,23 +5,23 @@ const AnimatedBackground = () => {
   return (
     <div className="absolute inset-0 overflow-hidden pointer-events-none">
       <div className="relative w-full h-full">
-        <div className="absolute left-[5%] sm:left-[10%] md:left-[16%] top-[50%] sm:top-[85%] md:top-[85%] -translate-y-1/2 animate-float-left">
+        <div className="absolute left-[2%] sm:left-[5%] md:left-[10%] top-[85%] md:top-[85%] -translate-y-1/2 animate-float-left">
           <Image
-            src="/images/vector.svg"
+            src="/images/communication-left.svg"
             alt="Communication Left"
             width={400}
             height={400}
-            className="w-[80px] h-[80px] xs:w-[500px] xs:h-[500px] sm:w-[250px] sm:h-[250px] md:w-[250px] md:h-[250px] lg:w-[400px] lg:h-[400px] opacity-100"
+            className="w-[120px] h-[120px] sm:w-[200px] sm:h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] opacity-90"
             priority
           />
         </div>
-        <div className="absolute right-[5%] sm:right-[10%] md:right-[10%] top-[50%] sm:top-[85%] md:top-[85%] -translate-y-1/2 animate-float-right">
+        <div className="absolute right-[2%] sm:right-[5%] md:right-[10%] top-[85%] md:top-[85%] -translate-y-1/2 animate-float-right">
           <Image
-            src="/images/vector-1.svg"
+            src="/images/communication-right.svg"
             alt="Communication Right"
             width={400}
             height={400}
-            className="w-[80px] h-[80px] xs:w-[500px] xs:h-[500px] sm:w-[150px] sm:h-[150px] md:w-[250px] md:h-[250px] lg:w-[400px] lg:h-[400px] opacity-90"
+            className="w-[120px] h-[120px] sm:w-[200px] sm:h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] opacity-90"
             priority
           />
         </div>
