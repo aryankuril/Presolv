8e9541286e58cec67d663f9113098a71aac07891
Aryan Kuril <160471508+aryankuril@users.noreply.github.com>
2025-06-12 10:51:04 +0530

    project added

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect, useCallback } from 'react';
 import Link from 'next/link';
+// import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { 
   signInWithEmailAndPassword, 
